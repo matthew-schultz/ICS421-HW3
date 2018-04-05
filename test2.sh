349b037919259743d1e29d3d67067fa518bdf037
ms <mschultz@hawaii.edu>
2018-04-05 12:01:54 -1000

    finish working version of test2

--- test2.sh
+++ test2.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 test-cases/test2/test2-ms-1.pre
-python3 runSQL.py test-cases/test2/test2-ms-1.cfg test-cases/test2/test2-ms-1.csv >  test-cases/test2/test2-ms-1.out
-# test-cases/test1/test1-ms-1.post | sort > test-cases/test1/test1-ms-1.post.out
-# DIFF=$(diff test-cases/test1/test1-ms-1.post.out test-cases/test1/test1-ms-1.post.exp)
-test-cases/test2/test2-ms-1.post
+python3 runSQL.py test-cases/test2/test2-ms-1.cfg test-cases/test2/test2-ms-1.csv > test-cases/test2/test2-ms-1.out
+# test-cases/test2/test2-ms-1.post | sort > test-cases/test2/test2-ms-1.post.out
+test-cases/test2/test2-ms-1.post | sort > test-cases/test2/test2-ms-1.post.out
+DIFF=$(diff test-cases/test2/test2-ms-1.post.out test-cases/test2/test2-ms-1.post.exp)
 if [ "$DIFF" != "" ] 
 then
     echo "Test failed; output file did not match"
+    echo $DIFF
 else
     echo "Test ran successfully; output files matched"
+    # rm test-cases/test2/test2-ms-1.post.out
 fi
