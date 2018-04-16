91654d981a01e7ea97c76967769b6e1b47240d08
ms <mschultz@hawaii.edu>
2018-04-15 19:07:32 -1000

    remove test4 db files

--- test4.sh
+++ test4.sh
@@ -4,7 +4,7 @@
 test-cases/test4/test4-ms-1.pre
 #python3 runSQL.py test-cases/test4/test4-ms-1.cfg test-cases/test4/test4-ms-1.sql | sort > test-cases/test4/test4-ms-1.out
 python3 runSQL.py test-cases/test4/test4-ms-1.cfg test-cases/test4/test4-ms-1.sql > test-cases/test4/test4-ms-1.out
-#test-cases/test4/test4-ms-1.post #| sort > test-cases/test4/test4-ms-1.post.out
+test-cases/test4/test4-ms-1.post #| sort > test-cases/test4/test4-ms-1.post.out
 #DIFF=$(diff test-cases/test4/test4-ms-1.out test-cases/test4/test4-ms-1.out.exp)
 if [ "$DIFF" != "" ] 
 then
